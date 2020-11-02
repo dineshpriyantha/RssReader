3435cf229f572caaa200ec797357eec71da497f5
dineshpriyantha <dinesh.priyantha.uom@gmail.com>
2020-11-02 23:50:26 +0900

    Add project files.

---
+++ RssReader/ViewModel/FakeRssHelper.cs
@@ -0,0 +1,31 @@
+﻿using RssReader.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RssReader.ViewModel
+{
+    public class FakeRssHelper : IRssHelper
+    {
+        public List<Item> GetPosts()
+        {
+            List<Item> items = new List<Item>();
+
+            items.Add(new Item()
+            {
+                Title ="title 01",
+                PubDate = "Thu, 22 Nov 2018 6:13:30 GMT"
+            });
+
+            items.Add(new Item()
+            {
+                Title = "Title 02",
+                PubDate = "Fri, 23 Nov 2018 9:27:14 GMT"
+            });
+
+            return items;
+        } 
+    }
+}
